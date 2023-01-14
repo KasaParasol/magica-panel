41c6344ef77a5c8d2c73d695262a59acbfe0868c
KasaParasol <yanase.takuma.oct+tsukasa@gmail.com>
2023-01-14 14:56:17 +0900

    refactor: lint

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,18 +1,16 @@
 const path = require('node:path');
 
-module.exports = env => {
-    return {
-        entry: './src/index.js',
-        output: {
-            path: env.mode === "demo"? path.resolve('./demo'): path.resolve('./dist'),
-            filename: 'main.js',
-            library: {
-                name: 'MagicaPanel',
-                type: 'var',
-                export: 'default',
-            },
+module.exports = env => ({
+    entry: './src/index.js',
+    output: {
+        path: env.mode === 'demo' ? path.resolve('./demo') : path.resolve('./dist'),
+        filename: 'main.js',
+        library: {
+            name: 'MagicaPanel',
+            type: 'var',
+            export: 'default',
         },
-        mode: 'development',
-        devtool: 'inline-source-map',
-    };
-};
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+});
